bc43ed2d85710f312beb3f0785731cc83ed2bc6f
Fazzelis <odidk08@gmail.com>
2025-11-21 20:39:29 +0700

    Добавлен файл для сессий подключений к бд

---
+++ backend/database/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+engine = create_async_engine(os.getenv("DATABASE_URL"), echo=True)
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
+Base = declarative_base()
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session

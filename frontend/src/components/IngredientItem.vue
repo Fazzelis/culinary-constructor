18c58f8571f5ca998592c2298527c3b22136e25f
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-11-22 23:09:31 +0700

    создан компонент IngredientItem

---
+++ frontend/src/components/IngredientItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li
+    class="category__item"
+    @click="$emit('toggleIngredient', ingredient)"
+    :class="{
+      'category__item--selected': selectedIngredients.includes(ingredient.id),
+    }"
+  >
+    {{ ingredient.name }}
+  </li>
+</template>
+
+
+<script>
+export default {
+  props: {
+    ingredient: {
+      type: Object,
+      required: true,
+    },
+    selectedIngredients: {
+        type: Object,
+        required: true,
+    }
+  },
+};
+</script>

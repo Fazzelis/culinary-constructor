8ec9cfc58a9138d08d3a6182858f466f4f95b2ac
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-11-22 23:12:32 +0700

    подключен PageHeader в главный компонент и сделано отображение страниц через router-view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  
+  <div>
+    <PageHeader />
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script>
+import PageHeader from "./components/PageHeader.vue";
+
+export default {
+  components: { PageHeader },
+};
+</script>
+
+<style lang="less">
+
+</style>

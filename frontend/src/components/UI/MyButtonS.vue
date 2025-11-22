cca0898905ffc3442f24e297428f493ccf40c4c8
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-11-22 23:06:05 +0700

    создан компонен MyButtonS для кнопок с иконками

---
+++ frontend/src/components/UI/MyButtonS.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="button-s" title="Найти блюдо">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="less">
+.button-s {
+  padding: 7px;
+  gap: 8px;
+  border-radius: 4px;
+  border: 3px solid @orange;
+  background-color: @orange;
+
+  &:hover {
+    background-color: @light;
+
+    svg {
+      color: @orange;
+    }
+  }
+
+  &:focus-visible {
+    background-color: @light;
+
+    svg {
+      color: @orange;
+    }
+  }
+}
+</style>

4acff01b4dbd6a65d1bc572791583341df7c2d98
Fazzelis <odidk08@gmail.com>
2025-11-22 15:28:16 +0700

    Подключен router категории к основному файлу

--- backend/controllers/routers.py
+++ backend/controllers/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from controllers.color_controller import router as color_router
+from controllers.category_controller import router as category_router
 
 
 router = APIRouter()
 router.include_router(color_router)
+router.include_router(category_router)

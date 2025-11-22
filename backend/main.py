63ef3668b30b9894f1246ca62440d1bb8cffe315
Fazzelis <odidk08@gmail.com>
2025-11-22 12:00:32 +0700

    Подключил общий router в app

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from database.database import engine, Base
 from fastapi.middleware.cors import CORSMiddleware
+from models.category import Category
+from models.color import Color
+from models.ingredient import Ingredient
+from controllers.routers import router
 
 app = FastAPI()
+app.include_router(router)
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
@@ -16,4 +21,4 @@ app.add_middleware(
 @app.on_event("startup")
 async def create_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadate.create_all)
+        await conn.run_sync(Base.metadata.create_all)

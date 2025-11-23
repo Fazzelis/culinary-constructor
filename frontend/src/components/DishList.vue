ca5ca918cb0c8339842e0c2d07fe59e229b94f7e
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-11-23 22:47:55 +0700

    создан комопнент DishList

---
+++ frontend/src/components/DishList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="listing">
+    <dish-card v-for="dish in dishList" :key="dish.id" :dish="dish" />
+  </div>
+</template>
+
+<script>
+import DishCard from "./DishCard.vue";
+
+export default {
+  components: { DishCard },
+  props: {
+    dishList: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.listing {
+  display: flex;
+  flex-direction: column;
+  row-gap: 40px;
+}
+</style>

7fb758026f402e4ab46975a924f7ea4daf0614a6
Fazzelis <odidk08@gmail.com>
2025-11-25 01:16:00 +0700

    Добавлена response_model

--- backend/controllers/dish_controller.py
+++ backend/controllers/dish_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from schemas.request.dish_request import DishRequestSchema
 from dependencies import get_dish_service
 from services.dish_service import DishService
+from schemas.response.dish_response import DishResponseSchema
 
 
 router = APIRouter(
@@ -10,7 +11,7 @@ router = APIRouter(
 )
 
 
-@router.post("")
+@router.post("", response_model=DishResponseSchema)
 async def create_dish(
         payload: DishRequestSchema,
         dish_service: DishService = Depends(get_dish_service)

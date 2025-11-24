fa5b40b9fb11a674e6d2b043c53d3ea39fef7e32
Fazzelis <odidk08@gmail.com>
2025-11-24 19:50:43 +0700

    Подключен router шага рецепта к основному файлу

--- backend/controllers/routers.py
+++ backend/controllers/routers.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from controllers.color_controller import router as color_router
 from controllers.category_controller import router as category_router
 from controllers.ingredient_controller import router as ingredient_router
+from controllers.recipe_step_controller import router as recipe_step_router
 
 
 router = APIRouter()
 router.include_router(color_router)
 router.include_router(category_router)
 router.include_router(ingredient_router)
+router.include_router(recipe_step_router)

4283bbaa99e5ac69fbd50823452ce354cf9611e6
ElenaShabaeva <shabaevalenatomsk@gmail.com>
2025-12-03 22:56:24 +0700

    изменен компонент CaloriesItem (добавлена иконка)

--- frontend/src/components/CaloriesItem.vue
+++ frontend/src/components/CaloriesItem.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="calories">
     <div class="calories__item">
-      <span class="calories__name">{{ calories.name }}</span>
+      <span class="calories__name">{{ item.name }}</span>
       <div class="calories__icon">
-        <slot></slot>
+        <component :is="iconComponent" />
       </div>
       <div class="calories__counter">
-        <span class="calories__counter calories__counter--m">{{ calories.counterNum }}</span>
-        <span class="calories__counter calories__counter--s">{{ calories.counterText }}</span>
+        <span class="calories__counter calories__counter--m">{{ item.counterNum }}</span>
+        <span class="calories__counter calories__counter--s">{{ item.counterText }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import SvgCalories from '../assets/icons/SvgCalories.vue';
+import SvgCarbs from '../assets/icons/SvgCarbs.vue';
+import SvgFats from '../assets/icons/SvgFats.vue';
+import SvgProtein from '../assets/icons/SvgProtein.vue';
+
 export default {
+  components: {SvgCalories, SvgCarbs, SvgFats, SvgProtein},
   props: {
-    calories: {
+    item: {
       type: Object,
       required: true
-    }
+    },
+    iconComponent: { type: Object, required: true }
   }
 }
 </script>

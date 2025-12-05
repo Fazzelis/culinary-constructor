fea8c63bfa6f9d2cb38d929c93efb9528a39a47a
Fazzelis <odidk08@gmail.com>
2025-12-06 01:55:22 +0700

    Добавлен поиск по названию

--- backend/controllers/dish_controller.py
+++ backend/controllers/dish_controller.py
@@ -46,3 +46,13 @@ async def get_dishes_by_ingredients(
         dish_service: DishService = Depends(get_dish_service)
 ):
     return await dish_service.get_dishes_by_ingredients(ingredients=ingredients, page=page, page_size=page_size)
+
+
+@router.get("/search-by-name", response_model=DishesResponseSchema)
+async def get_dishes_by_name(
+        dish_name: str,
+        page: int = Query(1, ge=1, description="Номер страницы"),
+        page_size: int = Query(5, ge=1, le=100, description="Количество элементов на одной странице"),
+        dish_service: DishService = Depends(get_dish_service)
+):
+    return await dish_service.get_dishes_by_name(name=dish_name, page=page, page_size=page_size)
